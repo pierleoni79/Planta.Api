04ff973900b3ed6369165f6ee17e7c609a2fa637
Angel David Cervantes Castillo <pierleoni@hotmail.com>
2025-10-24 23:06:44 -0500

    cambio

---
+++ Planta.Contracts/Procesos/ProcesarTrituracionRequest.cs
@@ -0,0 +1,10 @@
+﻿// Ruta: /Planta.Contracts/Procesos/ProcesarTrituracionRequest.cs | V1.0
+namespace Planta.Contracts.Procesos;
+
+public sealed class ProcesarTrituracionRequest
+{
+    public double PesoEntrada { get; set; }       // obligatorio
+    public double PesoSalida { get; set; }       // obligatorio
+    public double Residuos { get; set; }       // opcional (0 si no aplica)
+    public string? Observaciones { get; set; }
+}
